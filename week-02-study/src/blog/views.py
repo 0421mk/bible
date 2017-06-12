3298cca58b42b71a4776fe28de6a2a351cbb7e3f
0421mk <0421mk@naver.com>
2017-06-12 23:26:18 +0900

    I Know

--- week-02-study/src/blog/views.py
+++ week-02-study/src/blog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!!!You're at the polls index.")
+    return HttpResponse("나는  장고의 가장 기본이 되는 원리를 깨우쳐버렸다...")
